77e2481435d35d0f613a1ea4fb392b68dae7c109
Steve Fau <stepan.fau@gmail.com>
2023-10-27 22:01:09 +0200

    props, CSS update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
+import profilePic from './assets/ProfilePicture.png'
+import orcProfilePic from './assets/OrcProfilePic.png'
+import steveProfilePic from './assets/SteveProfilePic.png'
 
 function App() {
 
   return(
     <>
       <Header/>
-      <Card />
+      <Card name="Fauste1" description="I write code." image={profilePic}/>
+      <Card name="Hexsus" description="I play videogames." image={orcProfilePic}/>
+      <Card name="Steve" description="I do stuff." image={steveProfilePic}/>
       <Card />
       <Footer/>
     </>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,27 @@
 
-import profilePic from './assets/ProfilePicture.png'
+import PropTypes from 'prop-types'
+import defaultProfilePic from './assets/defaultProfilePic.png'
 
-function Card() {
+function Card(props) {
     return (
         <div className='card'>
-            <img src={profilePic} alt="Profile picture" />
-            <h2 className='card-title'>Fauste1</h2>
-            <p className='card-text'>I do stuff & write code.</p>
+            <img src={props.image} alt="Profile picture" />
+            <h2 className='card-title'> {props.name} </h2>
+            <p className='card-text'> {props.description} </p>
         </div>
     );
+}
+
+Card.propTypes = {
+    name: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string
+}
+
+Card.defaultProps = {
+    name: "Guest",
+    description: "I'm new here.",
+    image: defaultProfilePic
 }
 
 export default Card
